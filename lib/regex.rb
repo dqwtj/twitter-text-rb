213f48a3c24fa448f9d12d11b7e64ba1d57ba50f
Hooopo <hoooopo@gmail.com>
2011-03-18 18:34:40 +0800

    修改话题正则

--- lib/regex.rb
+++ lib/regex.rb
@@ -49,7 +49,7 @@ module Twitter
     REGEXEN[:end_screen_name_match] = /^(?:#{REGEXEN[:latin_accents]}|:\/\/)/o
     # Characters considered valid in a hashtag but not at the beginning, where only a-z and 0-9 are valid.
     HASHTAG_CHARACTERS = /[a-z0-9_#{LATIN_ACCENTS}]/io
-    REGEXEN[:auto_link_hashtags] = /(.*?)(#|＃)(.{1,20})(#|＃)/io
+    REGEXEN[:auto_link_hashtags] = /(.*?)(#|＃)(.{1,20}?)(#|＃)/io
     REGEXEN[:auto_link_usernames] = /(.*?)([@＠]+)([a-zA-Z0-9_]{1,20})/o
     REGEXEN[:auto_link_emoticon] = /(8\-\#|8\-E|\+\-\(|\`\@|\`O|\&lt;\|:~\(|\}:o\{|:\-\[|\&gt;o\&lt;|X\-\/|\[:-\]\-I\-|\/\/\/\/Ö\\\\\\\\|\(\|:\|\/\)|∑:\*\)|\( \| \))/
 
